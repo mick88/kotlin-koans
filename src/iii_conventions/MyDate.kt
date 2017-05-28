347bd0bf358885f83da4a4589a9611572ca8cbb6
Michal Dabski <michaldabski@gmail.com>
2017-05-28 18:35:35 +0100

    Solve 25

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,12 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.lang.Comparable
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : kotlin.Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return (dayOfMonth - other.dayOfMonth) + (month - other.month) * 31 + (year - other.year) * 366;
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
