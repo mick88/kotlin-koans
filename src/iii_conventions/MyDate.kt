f1dc5911cf99ffb522ea23ee31e5d8686edc9971
Michal Dabski <michaldabski@gmail.com>
2017-05-28 20:43:28 +0100

    Solve 29

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,10 +1,18 @@
 package iii_conventions
 
-import java.lang.Comparable
+import kotlin.coroutines.experimental.EmptyCoroutineContext.plus
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : kotlin.Comparable<MyDate> {
     override fun compareTo(other: MyDate): Int {
         return (dayOfMonth - other.dayOfMonth) + (month - other.month) * 31 + (year - other.year) * 366;
+    }
+
+    operator fun plus(interval: TimeInterval): MyDate {
+        return addTimeIntervals(interval, 1)
+    }
+
+    operator fun plus(interval: TimeIntervalMultiplier): MyDate {
+        return addTimeIntervals(interval.interval, interval.times)
     }
 }
 
@@ -13,7 +21,15 @@ operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other);
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR,
+}
+
+data class TimeIntervalMultiplier(val interval: TimeInterval, val times: Int) {
+
+}
+
+operator fun TimeInterval.times(num: Int): TimeIntervalMultiplier {
+    return TimeIntervalMultiplier(this, num)
 }
 
 class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
