0dcab87662848c01a639d355922297bd42e0e6cd
Michal Dabski <michaldabski@gmail.com>
2017-11-15 21:42:58 +0000

    Solve 34

--- src/iv_properties/_34_DelegatesExamples.kt
+++ src/iv_properties/_34_DelegatesExamples.kt
@@ -2,9 +2,21 @@ package iv_properties
 
 import util.TODO
 import util.doc34
+import kotlin.reflect.KProperty
+
+class Delegate(val initializer: () -> Int) {
+    var value : Int? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        if (value == null) {
+            value = initializer()
+        }
+        return value!!
+    }
+}
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    val lazyValue: Int by Delegate(initializer)
 }
 
 fun todoTask34(): Lazy<Int> = TODO(
